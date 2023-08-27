64a3baee7c6ffb150b6043d1630019c5eb23a8f6
Ephmeral <2669128599@qq.com>
2023-08-27 20:40:54 +0800

    Init: connect to mysql

---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/Ephmeral/TodoList/conf"
+)
+
+func main() {
+	conf.Init()
+}
---
+++ model/init.go
@@ -0,0 +1,30 @@
+package model
+
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/jinzhu/gorm"
+	"time"
+)
+
+var DB *gorm.DB
+
+func Database(conn string) {
+	//fmt.Println(conn)
+	db, err := gorm.Open("mysql", conn)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("MySQL数据库连接成功")
+	db.LogMode(true)
+	if gin.Mode() == "release" {
+		db.LogMode(false)
+	}
+
+	sqlDB := db.DB()
+	sqlDB.SetMaxIdleConns(20)  //设置连接池，空闲
+	sqlDB.SetMaxOpenConns(100) //打开
+	sqlDB.SetConnMaxLifetime(time.Second * 30)
+	DB = db
+}
